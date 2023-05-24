351aa7df4287ad39e9e7afb4b05454c53bcb4028
yeduruswapna <swapnayeduru@gmail.com>
2023-05-24 17:17:31 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(){
+        RWOnly rwonly= new RWOnly();
+        rwonly.setName("Swapna");
+        String name= rwonly.getName();
+    }
 }
